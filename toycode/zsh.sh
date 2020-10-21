5d4b841e69dd0009350a90858adcfb73ff9c3f6a
jeonhobeom <37523098+HoBeom@users.noreply.github.com>
2020-10-21 10:18:19 +0900

    Create zsh.sh
    ENV Backup

---
+++ toycode/zsh.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+sudo apt install -y zsh wget curl git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt install fonts-powerline
+# ZSH_THEME="agnoster"

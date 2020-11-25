cab7f42a58ac7526208a2a92daa575e9be21428b
jeonhobeom <37523098+HoBeom@users.noreply.github.com>
2020-11-25 22:23:33 +0900

    Update plot.py

--- toycode/plot.py
+++ toycode/plot.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from glob import glob
 filenames = glob('./total/*')
 for idx, filename in tqdm(enumerate(filenames)):
-    waveform, sample_rate = torchaudio.load(filename)=
+    waveform, sample_rate = torchaudio.load(filename)
     plt.figure()
     plt.plot(waveform.t().numpy())
     plt.savefig('./plotwave/{}.png'.format(idx))

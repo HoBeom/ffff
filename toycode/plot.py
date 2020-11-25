30c6a4efbd4afe1cddb0151c9a44e8b5c7662f82
jeonhobeom <37523098+HoBeom@users.noreply.github.com>
2020-11-25 22:22:52 +0900

    Update plot.py

--- toycode/plot.py
+++ toycode/plot.py
@@ -5,14 +5,8 @@ from tqdm import tqdm
 from glob import glob
 filenames = glob('./total/*')
 for idx, filename in tqdm(enumerate(filenames)):
-    waveform, sample_rate = torchaudio.load(filename)
-
-    # print("Shape of waveform: {}".format(waveform.size()))
-    # print("Sample rate of waveform: {}".format(sample_rate))
-
+    waveform, sample_rate = torchaudio.load(filename)=
     plt.figure()
     plt.plot(waveform.t().numpy())
     plt.savefig('./plotwave/{}.png'.format(idx))
     plt.close()
-
-# read error in RIFF header not found
